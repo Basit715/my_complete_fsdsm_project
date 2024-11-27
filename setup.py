b203957aaa0dde847b3bf2bdfad5db9a95194121
Basit715 <basitabass27411@gmail.com>
2024-11-27 17:04:48 +0530

    installed the local package successfully

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+     name="DiamondPricePrediction",
+     version="0.0.1",
+     author="Basit Pushoo",
+     author_email="basitabass27411@gmail.com",
+     # install_requires = get_requirements('requirements.txt')
+     packages=find_packages()
+)
