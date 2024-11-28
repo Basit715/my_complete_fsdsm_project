1a67f9c09f5ceb6145f48ca70ab452ceff3667f5
Basit715 <basitabass27411@gmail.com>
2024-11-28 18:47:02 +0530

    completed data ingestion and data transformation files

--- src/DiamondPricePrediction/components/data_ingestion.py
+++ src/DiamondPricePrediction/components/data_ingestion.py
@@ -1,10 +1,55 @@
+import pandas as pd
+import numpy as np
+import os
+import sys
+
 from src.DiamondPricePrediction.logger import logging
+from src.DiamondPricePrediction.exception import customException
+
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+from pathlib import Path
+
+
+class DataIngestionConfig:
+          raw_data_path = os.path.join("artifacts", "raw.csv")
+          train_data_path = os.path.join("artifacts", "train.csv")
+          test_data_path = os.path.join("artifacts", "test.csv")
 
 class DataIngestion:
      def __init__(self):
-          pass
+          self.ingestion_config = DataIngestionConfig()
      
-class InitiateDataIngestion:
-     def __init__(self):
-          pass
+     def initiate_data_ingestion(self):
+          logging.info("Data ingestion started")
+          
+          
+          try:
+               my_data = pd.read_csv(Path(os.path.join("notebooks/data", "train.csv")))
+               logging.info("Read the dataset as a dataframe")
+               
+               
+               os.makedirs(os.path.dirname(os.path.join(self.ingestion_config.raw_data_path)), exist_ok=True)
+               my_data.to_csv(self.ingestion_config.raw_data_path, index=False)
+               logging.info("saved the raw data in artifacts folder")
+               
+               
+               logging.info("perform train test split")
+               train_data,test_data = train_test_split(my_data, test_size=0.25,random_state=25)
+               logging.info("train test split completed")
+               
+               train_data.to_csv(self.ingestion_config.train_data_path, index=False)
+               test_data.to_csv(self.ingestion_config.test_data_path, index=False)
+               
+               logging.info("Data ingestion part completed")
+               
+               return(
+                    self.ingestion_config.train_data_path,
+                    self.ingestion_config.test_data_path
+               )
+               
+          except Exception as e:
+               logging.info("Exception occured at the data ingestion stage")
+               raise customException(e,sys)
      
--- src/DiamondPricePrediction/components/data_transformation.py
+++ src/DiamondPricePrediction/components/data_transformation.py
@@ -1,13 +1,97 @@
+import pandas as pd
+import numpy as np
+import os
+import sys
 
 
+from src.DiamondPricePrediction.logger import logging
+from src.DiamondPricePrediction.exception import customException
 
 
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OrdinalEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+
+from src.DiamondPricePrediction.utils.utils import save_object
+
+
+from dataclasses import dataclass
+
+@dataclass
+class DataTransformationConfiguration:
+     preproccessor_obj_path = os.path.join("artifacts", 'preprocessor.pkl')
 
 
 class DataTransformation:
      def __init__(self):
-          pass
+          self.transformation_config = DataTransformationConfiguration()
      
-class get_data_transformation:
-     def __init__(self):
-          pass
+     def get_data_transformation(self):
+          try:
+               logging.info("Data transformation initiated")
+               
+               cat_cols = ['cut', 'color', 'clarity']
+               num_cols = ['carat', 'depth', 'table', 'x', 'y', 'z']
+               
+               logging.info("pipeline initiated")
+               num_pipeline = Pipeline(
+                    steps=[
+                         ('impute', SimpleImputer(strategy='mean'))
+                         ('scaler', StandardScaler())
+                    ]
+               )
+               cat_pipeline = Pipeline(
+                    steps=[
+                         ('imputer', SimpleImputer(strategy='most_frequent')),
+                         ('encoder', OrdinalEncoder())
+                    ]
+               )
+               
+               preproccessor = ColumnTransformer(
+                    transformers=[
+                         ('numerical_pipeline', num_pipeline, num_cols),
+                         ('categorical_pipeline', cat_pipeline, cat_cols)
+                    ]
+               )
+               return preproccessor
+               
+          except Exception as e:
+               logging.info("Exception occured in get data transformation function of data transformation file")
+               raise customException(e,sys)
+          
+     def initiate_data_transformation(self, train_path,test_path):
+          try:
+               train_df = pd.read_csv(train_path)
+               test_df = pd.read_csv(test_path)
+               logging.info("I have read the train and test data path")
+               logging.info("Train dataframe head \n",train_df.head())
+               logging.info("test dataframe head \n", test_df.head())
+               
+               
+               
+               preprocceser_obj = self.get_data_transformation()
+               
+               target_coloum = "price"
+               drop_coloumns = [target_coloum, 'id']
+               
+               input_fetaure_train_df = train_df.drop(columns=drop_coloumns, axis=1)
+               input_feature_test_df = test_df.drop(columns=drop_coloumns, axis=1)
+               target_feature_train_df = train_df[target_coloum]
+               
+               
+               preprocceser_obj.fit_transform(input_fetaure_train_df)
+               preprocceser_obj.transform(input_feature_test_df)
+               
+               logging.info("Applying preproccesor object on train and test dataset")
+               
+               
+               
+               save_object(
+                    file_path = self.transformation_config.preproccessor_obj_path,
+                    obj = preprocceser_obj
+               )
+               
+          except Exception as e:
+               logging.info("Exception occured in initiate data transformation of data transformation file")
+               raise customException(e,sys)
